fa621987044200d9ba1af10f5b5605fc56b48b92
Gnovillo1120 <79721109+Gnovillo1120@users.noreply.github.com>
2022-02-07 23:44:10 +0000

    Initial commit

---
+++ airplane.py
@@ -0,0 +1,7 @@
+class Dog:
+  #Constructor Method
+  def __init__(self,whatColor,whatBreed,whatGender,whatAge):
+    self.color = whatColor
+    self.breed = whatBreed
+    self.gender = whatGender
+    self.age = whatAge
---
+++ main.py
@@ -0,0 +1,19 @@
+from airplane import Dog
+Guillermos_Dog = Dog("Black", "labrador", "female", 1 )
+print(Guillermos_Dog.color)
+print(Guillermos_Dog.breed)
+print(Guillermos_Dog.gender)
+print(Guillermos_Dog.age)
+
+
+
+
+
+
+
+
+
+
+
+
+
